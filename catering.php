dfb50aac4432cac5406e2e85c67f101032be284b
katelyn-raye <57073079+katelyn-raye@users.noreply.github.com>
2020-04-15 18:49:55 -0500

    fixed the goof up

---
+++ catering.php
@@ -0,0 +1,38 @@
+<head>
+    <meta charset="utf-8">
+
+    <title> Bella Vita Italian Restaurant | Catering </title>
+    <meta name="description" content="ADD SEO HERE">
+    <meta name="author" content="Katelyn Needham & Kathryn Kerr">
+
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+  <header>
+    <div class ="container-fluid-nav-bar">
+      <button type="button" class="navbar-toggler" data-target="#mainNavigation">
+          <i class="fa fa-bars"></i>
+      </button>
+
+    <!-- NAVIGATION-->
+      <div id="mainNavigation" class="collapse navbar-collapse">
+          <ul class="navbar-nav">
+              <li class="navbar-item">
+                  <a class="navbar-link" href="index.html" title="Home">Home</a>
+              </li>
+              <li class="navbar-item">
+                  <a class="navbar-link" href="#" title="Menu">Menu</a>
+              </li>
+              <li class="navbar-item">
+                  <a class="navbar-link" href="#" title="Catering">Catering</a>
+              </li>
+              <li class="navbar-item">
+                  <a class="navbar-link" href="contact.html" title="Contact">Contact</a>
+              </li>
+          </ul>
+      </div>
+    </div>
+    <!-- NAVIGATION End -->
+  </body>
+  
