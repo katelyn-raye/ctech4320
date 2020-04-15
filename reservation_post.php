85110c06b677dd24a7d39c18dae74c9ffd64bc56
kathrynkerr <57072945+kathrynkerr@users.noreply.github.com>
2020-04-15 16:05:02 -0500

    added post

---
+++ reservation_post.php
@@ -0,0 +1,23 @@
+<?php
+
+?>
+
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+
+    <link rel="stylesheet" href="master.css">
+
+  </head>
+  <body>
+    <h2>Preview Your Message</h2>
+
+    <hr size="1">
+    <p>
+    	<?php echo $output ?>
+    </p>
+  </body>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+  <script src="basic.js"></script>
+</html>
