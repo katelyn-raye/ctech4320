46671ed638f557554bd26f0f39341d696f255156
kathrynkerr <57072945+kathrynkerr@users.noreply.github.com>
2020-04-16 07:27:10 -0500

    got the css to work on the reservation

--- reservation_post.php
+++ reservation_post.php
@@ -1,4 +1,6 @@
 <?php
+include ('components.php');
+
 $fName = $_POST['fName'];
 $lNamee = $_POST['lName'];
 $partySize = $_POST['partySize'];
@@ -10,23 +12,21 @@ $bgcolor = $_POST['partyAccess'];
 $permitted_chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 $code = substr(str_shuffle($permitted_chars), 0, 5);
 
-$output="<p>Thank you $fName, we'll have a table ready for your party of $partySize at $time on $date. </p> <p>Your code is:</p> $code <p>Show this code to the host to check in.</p>";
+$output="<p>Thank you <span class='resEmphasis'>$fName</span>, we'll have a table ready for your party of <span class='resEmphasis'>$partySize</span> at <span class='resEmphasis'>$time</span> on <span class='resEmphasis'>$date</span>. </p> <p>Your code is:</p><span id='resCode'>$code</span> <p>Show this code to the host to check in.</p>";
 
 ?>
 
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title> Bella Vita Italian Restaurant | Online Reservation </title>
 
-    <link rel="stylesheet" href="master.css">
+    <link rel="stylesheet" href="css/styles.css">
 
   </head>
   <body>
-    <h2>Preview Your Message</h2>
-
-    <hr size="1">
     <p>
+      <?php echo makeHeader('header'); ?>
     	<?php echo $output ?>
     </p>
   </body>
